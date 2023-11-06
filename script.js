9ee1538b4a7193951f4058ceb5a7831c37d2b50c
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-06 22:14:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+let btn=document.getElementById("submit");
+btn.addEventListener("click",(e)=>{
+	e.preventDefault()
+	let red=document.getElementById("radius");
+	let vol=document.getElementById("volume");
+	if(red.value!==""){
+	let a=red.value;
+	let b=(4/3)*3.14*(a*a*a);
+	vol.value=b;
+	}
+})
